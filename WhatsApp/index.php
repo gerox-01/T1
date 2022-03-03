0bb40adee8b112fa2e97e84748be5bddd0179426
David Gerónimo Quiroga Torres <gedaquito@gmail.com>
2022-03-02 23:58:38 -0500

    Se termina estilo de WPP ✅

--- WhatsApp/index.php
+++ WhatsApp/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="aaa.css">
+    <link rel="stylesheet" href="estilos.css">
 
     <title>WhatsApp</title>
 </head>
@@ -14,10 +14,133 @@
 <body>
 
     <div class="bg-verde"></div>
+
     <div class="bg-wpp">
         <div class="container">
-            <div class="topbar"></div>
+
+            <div class="topbar">
+                <div class="section-profile">
+                    <div>
+                        <img class="foto-perfil" src="profile.png" alt="Profile Photo">
+                    </div>
+                    <div class="section-icons">
+                        <img class="status" src="status.png" alt="Status">
+                        <img class="newchat" src="newchat1.png" alt="New Chat">
+                        <img class="menu" src="menu.png" alt="Menu">
+                    </div>
+                </div>
+            </div>
+
+            <div class="content">
+
+                <div class="l-bar">
+                    <div class="search">
+                        <div class="section-search">
+                            <div class="search-bar">
+                                <img class="icon-search" src="search.png" alt="Search Icon">
+                                <input type="text" placeholder="Search or start new chat">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Contacts whastapp -->
+                    <div class="contacts">
+                        <div class="section-contacts">
+                            <div class="contact">
+                                <div class="contact-photo">
+                                    <img class="photo-contact" src="profile.png" alt="Contact Photo">
+                                </div>
+                                <div class="contact-info">
+                                    <div class="contact-name">
+                                        <h3 style="font-size: 1.5rem; padding-left: 30px">Universidad de Cundinamarca</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="contact">
+                                <div class="contact-photo">
+                                    <img class="photo-contact" src="profile.png" alt="Contact Photo">
+                                </div>
+                                <div class="contact-info">
+                                    <div class="contact-name">
+                                        <h3 style="font-size: 1.5rem; padding-left: 30px">Gabriel Rojas</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="contact">
+                                <div class="contact-photo">
+                                    <img class="photo-contact" src="profile.png" alt="Contact Photo">
+                                </div>
+                                <div class="contact-info">
+                                    <div class="contact-name">
+                                        <h3 style="font-size: 1.5rem; padding-left: 30px">Estudiantes</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="chats">
+                    <!-- <div class="chat">
+                        <div class="section-chat">
+                            <div class="chat-header">
+                                <div class="chat-header-info">
+                                    <div class="chat-header-photo">
+                                        <img class="photo-chat" src="profile.png" alt="Chat Photo">
+                                    </div>
+                                    <div class="chat-header-name">
+                                        <p>Universidad de Cundinamarca</p>
+                                    </div>
+                                </div>
+                                <div class="chat-header-icons">
+                                    <img class="icon-chat" src="icon-chat.png" alt="Icon Chat">
+                                    <img class="icon-video" src="icon-video.png" alt="Icon Video">
+                                    <img class="icon-audio" src="icon-audio.png" alt="Icon Audio">
+                                    <img class="icon-file" src="icon-file.png" alt="Icon File">
+                                </div>
+                            </div>
+                            <div class="chat-body">
+                                <div class="chat-message">
+                                    <div class="chat-message-photo">
+                                        <img class="photo-message" src="profile.png" alt="Message Photo">
+                                    </div>
+                                    <div class="chat-message-text">
+                                        <p>Hola, como estas?</p>
+                                    </div>
+                                </div>
+                                <div class="chat-message">
+                                    <div class="chat-message-photo">
+                                        <img class="photo-message" src="profile.png" alt="Message Photo">
+                                    </div>
+                                    <div class="chat-message-text">
+                                        <p>Bien, gracias</p>
+                                    </div>
+                                </div>
+                                <div class="chat-message">
+                                    <div class="chat-message-photo">
+                                        <img class="photo-message" src="profile.png" alt="Message Photo">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
+                    <div class="content-msg">
+                        <p>Aquí van los mensajes</p>
+                        <p>✅✅</p>
+                    </div>
+                    <div class="input-msg">
+                        <img class="emoji" src="emoji.png" alt="Emoji">
+                        <img class="clip" src="clip.png" alt="clip">
+                        <input class="input-type" type="text" placeholder="Escriba un mensaje">
+                        <img class="send-input" src="send.png" alt="send">
+                    </div>
+                </div>
+
+            </div>
+
         </div>
+
+
     </div>
 
 
